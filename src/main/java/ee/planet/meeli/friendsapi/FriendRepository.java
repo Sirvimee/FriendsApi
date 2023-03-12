868f0199b3e3e06704c022bd64c3fd747975adf8
Meeli <meeli.sirvi@tptlive.ee>
2023-03-12 17:21:13 +0200

    First init

---
+++ src/main/java/ee/planet/meeli/friendsapi/FriendRepository.java
@@ -0,0 +1,8 @@
+package ee.planet.meeli.friendsapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendRepository extends JpaRepository<Friend, Integer> {
+}
---
+++ src/test/java/ee/planet/meeli/friendsapi/FriendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.planet.meeli.friendsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
